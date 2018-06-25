03a6b4bad08c2ed58254706e87cc04373e2ec819
MrNati <emekaananti@gmail.com>
2018-06-25 14:28:46 +0100

    Initial commit

--- designs.js
+++ designs.js
@@ -3,8 +3,22 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+$(document).ready(function() {
+    $("#sizePicker").submit(function makeGrid(displayGrid) {
+        $("table tr").remove();
+        var rows = $("#inputHeight").val();
+        var cols = $("#inputWeight").val();
+            for (var i = 1; i <= rows; i++) {
+                $("table").append("<tr></tr>");
+                for (var j = 1; j <= cols; j++) {
+                    $("tr:last").append("<td></td>");
+                    $("td").attr("class", "cells");
+                }
+            }
+        displayGrid.preventDefault();
+        $(".cells").click(function() {
+            var paint = $("#colorPicker").val();
+            $(this).css("background", paint);
+        });
+    });
+});
